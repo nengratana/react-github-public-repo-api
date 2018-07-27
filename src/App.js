186d6cff8a03df2eb5633796281cd189cddd7979
Ratana Limnararat <neng.ratana@gmail.com>
2018-07-27 20:39:12 +0700

    Add next button Change "start" state to "currentRepo"

--- src/App.js
+++ src/App.js
@@ -9,26 +9,40 @@ class App extends Component {
     data: [],
     isLoading: false,
     error: false,
-    start: 0,
+    currentRepo: 0,
     resultsPerPage: 10
   };
 
   componentDidMount() {
     const baseurl = "https://api.github.com/repositories?since=";
-    const url = baseurl + this.state.start;
+    const url = baseurl + this.state.currentRepo;
 
     fetch(url)
       .then(response => response.json())
       .then(data => this.setState({ data }));
   }
 
+  next = event => {
+    const { currentRepo, resultsPerPage } = this.state;
+    const nextRepo = currentRepo + resultsPerPage;
+    this.setState({ currentRepo: nextRepo });
+  };
+
   render() {
-    const { data, isLoading, error, start, resultsPerPage } = this.state;
+    const { data, isLoading, error, currentRepo, resultsPerPage } = this.state;
 
     return (
       <div className="App">
         <h1>Github Public Repositories</h1>
-        <Table data={data} start={start} resultsPerPage={resultsPerPage} />
+        <Table
+          data={data}
+          currentRepo={currentRepo}
+          resultsPerPage={resultsPerPage}
+        />
+        <button className="button">Back</button>
+        <button className="button" onClick={this.next}>
+          Next
+        </button>
       </div>
     );
   }
--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Table = props => {
-  const { data, start, resultsPerPage } = props;
+  const { data, currentRepo, resultsPerPage } = props;
   return (
     <div>
-      {data.slice(start, start + resultsPerPage).map(repo => {
+      {data.slice(currentRepo, currentRepo + resultsPerPage).map(repo => {
         return <p key={repo.id}>{repo.name}</p>;
       })}
     </div>
