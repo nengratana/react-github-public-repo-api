af2baa2dade71ecb9952c36ca47c3b4c1737dbfe
Ratana Limnararat <neng.ratana@gmail.com>
2018-07-31 17:26:35 +0700

    Add styling to Table and Button Moving loading and error handling to Table Component

--- src/App.js
+++ src/App.js
@@ -8,18 +8,21 @@ class App extends Component {
     data: [],
     isLoading: false,
     error: false,
-    currentRepo: 1000, //142583100,
+    currentRepo: 0,
     resultCount: 0,
     resultsPerPage: 10
   };
 
   componentDidMount() {
+    // Show loading while fetching data from Github API
     this.setState({ isLoading: true });
     const baseurl = "https://api.github.com/repositories?since=";
     const url = baseurl + this.state.currentRepo;
     fetch(url)
       .then(response => {
+        // Hide loading
         this.setState({ isLoading: false });
+        // Check for error
         if (!response.ok) {
           throw Error("Request Failed");
         }
@@ -30,6 +33,7 @@ class App extends Component {
         data => {
           this.setState({ data });
         },
+        // If error then change error state to True
         () => {
           this.setState({ error: true });
         }
@@ -38,48 +42,51 @@ class App extends Component {
 
   next = () => {
     const { resultCount, resultsPerPage } = this.state;
+    // Set mark for .Slice
     const nextRepo = resultCount + resultsPerPage;
     this.setState({ resultCount: nextRepo });
   };
 
   back = () => {
     const { resultCount, resultsPerPage } = this.state;
+    // Set mark for .Slice
     const prevRepo = resultCount - resultsPerPage;
     this.setState({ resultCount: prevRepo });
   };
 
   render() {
     const { data, isLoading, error, resultCount, resultsPerPage } = this.state;
+    // Find total no. of data
     const results = data.length;
     return (
       <div className="App">
         <h1>Github Public Repositories</h1>
-        {isLoading ? "Loading" : ""}
-        {error ? (
-          "Something wrong with Github API"
-        ) : (
+        <div className="repo">
           <Table
             data={data}
             resultCount={resultCount}
             resultsPerPage={resultsPerPage}
+            isLoading={isLoading}
+            error={error}
           />
-        )}
+          <div className="pagination">
+            {resultCount === 0 ? (
+              ""
+            ) : (
+              <button className="button" onClick={this.back}>
+                Back
+              </button>
+            )}
 
-        {resultCount === 0 ? (
-          ""
-        ) : (
-          <button className="button" onClick={this.back}>
-            Back
-          </button>
-        )}
-
-        {resultCount + resultsPerPage < results ? (
-          <button className="button" onClick={this.next}>
-            Next
-          </button>
-        ) : (
-          ""
-        )}
+            {resultCount + resultsPerPage < results ? (
+              <button className="button" onClick={this.next}>
+                Next
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
       </div>
     );
   }
--- src/Table.js
+++ src/Table.js
@@ -1,37 +1,46 @@
 import React from "react";
 
 const Table = props => {
-  const { data, resultCount, resultsPerPage } = props;
+  const { data, resultCount, resultsPerPage, isLoading, error } = props;
   return (
-    <table className="table repo">
+    <table className="table">
       <thead>
         <tr>
           <th>ID</th>
-          <th>Name</th>
+          <th>Repo Name</th>
           <th>Description</th>
           <th>Owner</th>
         </tr>
       </thead>
-      <tbody>
-        {data.slice(resultCount, resultCount + resultsPerPage).map(repo => {
-          return (
-            <tr key={repo.id}>
-              <th>{repo.id}</th>
-              <td>
-                <a href={repo.html_url} title={repo.name}>
-                  {repo.name}
-                </a>
-              </td>
-              <td>{repo.description}</td>
-              <td>
-                <a href={repo.owner.html_url} title={repo.owner.login}>
-                  {repo.owner.login}
-                </a>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
+      {isLoading ? (
+        <tbody className="loading">Fetching data from Github API ...</tbody>
+      ) : (
+        <tbody />
+      )}
+      {error ? (
+        <tbody className="error">Fetching data failed!</tbody>
+      ) : (
+        <tbody>
+          {data.slice(resultCount, resultCount + resultsPerPage).map(repo => {
+            return (
+              <tr key={repo.id}>
+                <td className="id">{repo.id}</td>
+                <td className="name">
+                  <a href={repo.html_url} title={repo.name}>
+                    {repo.name}
+                  </a>
+                </td>
+                <td className="desc">{repo.description}</td>
+                <td className="owner">
+                  <a href={repo.owner.html_url} title={repo.owner.login}>
+                    {repo.owner.login}
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      )}
     </table>
   );
 };
